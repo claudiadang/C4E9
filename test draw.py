5561d50f8e715f00350465a6645178e779848d65
Claudia Dang <claudiadang@Claudias-MacBook-Pro.local>
2017-04-17 20:54:13 +0700

    Initial commit

---
+++ test draw.py
@@ -0,0 +1,30 @@
+##from turtle import *
+##
+##
+##color("green" , "yellow")
+##begin_fill()
+##
+##for i in range (4):
+##        forward(200)
+##        left(90)
+##end_fill()
+
+##from turtle import *
+##speed (-1)
+##color("green" , "white")
+##begin_fill()
+##
+##for i in range (6):
+##    circle(80)
+##    left(360/6)
+##end_fill()
+
+##from turtle import *
+##speed (-1)
+##color("green" , "white")
+##begin_fill()
+##
+##for i in range (100):
+##    circle(80)
+##    left(360/100)
+##end_fill()
